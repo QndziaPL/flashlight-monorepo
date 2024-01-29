9b4b89f9d0fef59c31530fe05d8b66a7b6b577dd
Maksymilian Rogacewicz <maksymilian.rogacewicz@gmail.com>
2024-01-29 14:37:41 +0100

    commenting Game.tsx

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,34 +1,32 @@
-import { FC, useEffect, useRef, useState } from "react";
-import GameState from "../classes/GameState/GameState.ts";
-import PlayerInput from "../classes/PlayerInput/PlayerInput.ts";
-import { useSocket } from "../context/WebSocketContext.tsx";
+import { FC } from "react";
 
 export type GameProps = {};
 export const Game: FC<GameProps> = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [gameState, setGameState] = useState<GameState>();
-  const {client} = useSocket();
-
-  useEffect(() => {
-    if (canvasRef.current)
-      canvasRef.current.height = window.innerHeight;
-      canvasRef.current.width = window.innerWidth;
-      setGameState(new GameState({ canvasRef: canvasRef.current, playerInput: new PlayerInput(), client: client }));
-    }
-  }, []);
-
-  useEffect(() => {
-    const startGameLoop = () => {
-      gameState?.tick();
-
-      requestAnimationFrame(startGameLoop);
-    };
-
-    const frameId = requestAnimationFrame(startGameLoop);
-
-    return () => {
-      cancelAnimationFrame(frameId);
-    };
-  }, [gameState]);
-  return <div>{/*<canvas ref={canvasRef} />*/}</div>;
+  return null;
+  // const canvasRef = useRef<HTMLCanvasElement>(null);
+  // const [gameState, setGameState] = useState<GameState>();
+  // const {client} = useSocket();
+  //
+  // useEffect(() => {
+  //   if (canvasRef.current)
+  //     canvasRef.current.height = window.innerHeight;
+  //     canvasRef.current.width = window.innerWidth;
+  //     setGameState(new GameState({ canvasRef: canvasRef.current, playerInput: new PlayerInput(), client: client }));
+  //   }
+  // }, []);
+  //
+  // useEffect(() => {
+  //   const startGameLoop = () => {
+  //     gameState?.tick();
+  //
+  //     requestAnimationFrame(startGameLoop);
+  //   };
+  //
+  //   const frameId = requestAnimationFrame(startGameLoop);
+  //
+  //   return () => {
+  //     cancelAnimationFrame(frameId);
+  //   };
+  // }, [gameState]);
+  // return <div>{/*<canvas ref={canvasRef} />*/}</div>;
 };
